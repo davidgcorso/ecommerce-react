848e54dd9dc1879645372a513d0ab13a5916cea7
David Gonzalez <davidgcorso@gmail.com>
2023-05-29 13:01:49 -0400

    adding delete product in cart section

--- src/components/CartContent/CartElements.js
+++ src/components/CartContent/CartElements.js
@@ -5,7 +5,14 @@ import CartItemCounter from './CartItemCounter'
 
 const CartElements = () => {
   const images = require.context("../../img", true)
-  const {cart} = useContext(dataContext)
+  const {cart, setCart} = useContext(dataContext)
+  const deleteProduct = (id) => {
+    const foundId =cart.find((element) => element.id === id)
+    const newCart =cart.filter((element) => {
+      return element !== foundId
+    })
+    setCart(newCart)
+  }
   return cart.map((product) => {
       return (
         <div className="cartElements" key={product.id}>
@@ -13,6 +20,7 @@ const CartElements = () => {
           <h3 className="cartElements__name">{product.name}</h3>
           <CartItemCounter product={product} quanty={product.quanty} />
           <h4 className="cartElements__price">$ {product.price * product.quanty}</h4>
+          <h3 className='cartElements__delete' onClick={()=> deleteProduct(product.id)}>❌</h3>
         </div>
       )
     })
